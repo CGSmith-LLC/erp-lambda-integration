346ac33ec65585fd788a3c216bf70587d6685180
faheemsharif1296 <mfaheemsharif1296@gmail.com>
2021-06-11 01:11:19 +0500

    Added Parameter Store and ERP and EUS queues

--- lambda-functions/request-processor/request_processor.py
+++ lambda-functions/request-processor/request_processor.py
@@ -1,16 +1,35 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 
 ## Constants
 SQS = "sqs"
+PARAMETER_NAME = "sqs-queues-paths"
+SSM = "ssm"
 
 def lambda_handler(event, context):
 
-    temp_sqs = "https://sqs.us-east-2.amazonaws.com/406186376066/eus-testing-queue"
+    ssm_client = get_service_client(SSM)
+    if ssm_client:
+        parameter_value = json.loads(get_parameter(ssm_client, PARAMETER_NAME))
+        sqs_queues = parameter_value['SQS_QUEUES']
+    try:
+        EUS_QUEUE_URL = sqs_queues['EUS_QUEUE_URL']
+        EUS_QUEUE_NAME = sqs_queues['EUS_QUEUE_NAME']
+        ERP_QUEUE_URL = sqs_queues['ERP_QUEUE_URL']
+        ERP_QUEUE_NAME = sqs_queues['ERP_QUEUE_URL']
+    except Exception as e:
+        print("Error reading parameter store: ", e.message)
+        exit(1)
 
-    response = save_event_to_sqs(temp_sqs, event)
+    print(parameter_value)
 
-    print(response)
+    if event['type'] == EUS_QUEUE_NAME:
+        response = save_event_to_sqs(EUS_QUEUE_URL, event)
+        print(response)
+    elif event['type'] == ERP_QUEUE_NAME:
+        response = save_event_to_sqs(ERP_QUEUE_URL, event)
+        print(response)
 
     return {
         'status': 200,
@@ -27,11 +46,32 @@ def save_event_to_sqs(queue_url, message_body):
     message_request = sqs_client.send_message(QueueUrl=queue_url, MessageBody=message_str)
     return message_request
 
+def get_parameter(client, parameter_name):
+    """Fetch and return parameters from the parameters store"""
+    try:
+        params = client.get_parameter(Name=parameter_name)
+    except ClientError as e:
+        print("Received Error: %s", e)
+        return False
+    else:
+        try:
+            return json.loads(params['Parameter']['Value'])
+        except Exception as e:
+            print("Received Error: %s", e)
+            print("Json is malformed, Kindly provide a valid JSON")
+            return False
+
+
 
 if __name__ == "__main__":
     event = {
-        "temp": "value"
+        "type": "erp-queue"
+    }
+
+    event2 = {
+        "type": "eus-queue"
     }
     context = ""
     print(lambda_handler(event, context))
+    print(lambda_handler(event2, context))
     
