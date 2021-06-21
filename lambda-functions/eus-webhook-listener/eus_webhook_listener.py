947693580687c5b9f1efec25d0e79b520e24a580
faheemsharif1296 <mfaheemsharif1296@gmail.com>
2021-06-22 02:05:02 +0500

    Added complete webhook listener code

--- lambda-functions/eus-webhook-listener/eus_webhook_listener.py
+++ lambda-functions/eus-webhook-listener/eus_webhook_listener.py
@@ -1,10 +1,30 @@
 import json
 import xml.etree.ElementTree as ET
+import boto3
+import os
 
+SQS = 'sqs'
+SSM = 'ssm'
+PARAMETER_NAME = os.environ('SQS_QUEUE_PATHS')
 
 def lambda_handler(event, context):
     # TODO implement
-    # print("event --> ", event)
+    print("event --> ", event)
+
+    ssm_client = get_service_client(SSM)
+    if ssm_client:
+        parameter_value = json.loads(get_parameter(ssm_client, PARAMETER_NAME))
+        sqs_queues = parameter_value['SQS_QUEUES']
+    try:
+        WEBHOOK_EVENT_QUEUE_URL = sqs_queues['WEBHOOK_EVENT_QUEUE_URL']
+        WEBHOOK_EVENT_QUEUE_NAME = sqs_queues['WEBHOOK_EVENT_QUEUE_NAME']
+    except Exception as e:
+        print("Error reading parameter Store: ", e)
+        return {
+            'statusCode': 500,
+            'message': json.dumps('Failed to get parameters.')
+        }
+        exit(1)
 
     try:
         body = event['body']
@@ -15,14 +35,47 @@ def lambda_handler(event, context):
             'message': json.dumps("Failed to extract message body. Please contact your administrator.")
         }
     else:
-        parsed_xml = parse_xml(body)
-        print(parsed_xml)
+        text_map = parse_xml(body)
+        print(text_map)
 
+    try:
+        response = save_event_to_sqs(WEBHOOK_EVENT_QUEUE_URL, text_map)
+        print("Event Added: ", response)
+    except Exception as e:
+        print("Failed to put event to queue: ", e)
+        return {
+            'statusCode': 500,
+            'message': json.dumps("Failed to put event to SQS queue")
+        }
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Event added to SQS queue.')
     }
+
+def get_service_client(service_name):
+    return boto3.client(service_name)
+
+def save_event_to_sqs(queue_url, message_body):
+    sqs_client = get_service_client(SQS)
+    message_str = json.dumps(message_body)
+    message_request = sqs_client.send_message(QueueUrl=queue_url, MessageBody=message_str)
+    return message_request
+
+def get_parameter(client, parameter_name):
+    """Fetch and return parameters from the parameter store"""
+    try:
+        params = client.get_parameter(Name=parameter_name)
+    except Exception as e:
+        print("Recevied Error: %s", e)
+        return False
+    else:
+        try:
+            return json.loads(params["Parameter"]["Value"])
+        except Exception as e:
+            print("Received Error: %s", e)
+            print("JSON is malformed, Kindly provide a valid JSON")
+            return False
 
 
 def parse_xml(xml_data):
@@ -37,7 +90,7 @@ def parse_xml(xml_data):
 
     print("Parent Map:", parent_map)
     print("Text Map: ", text_map)
-    return et
+    return text_map
 
 
 if __name__ == "__main__":
