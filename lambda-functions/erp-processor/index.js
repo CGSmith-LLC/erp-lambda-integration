bf4094b0045651f8649dc6d957e57c21289c97f0
Chris Smith <chris@cgsmith.net>
2021-07-07 07:47:41 -0500

    rename index

--- lambda-functions/erp-processor/index.js
+++ lambda-functions/erp-processor/index.js
@@ -16,15 +16,15 @@ odoo.connect(function (err) {
     innerParams.push({
         'name': 'Name of crm.lead.name',
         'active': 1,
-        'contact_name': 'chris+nodejs@cgsmith.net',
+        'contact_name': 'chris+nodejs@email.com',
         'type': 'lead',
-        'website': 'https://www.towercoverage.com/En-US/eus/eusdatadetails/1401403', // @todo this is the towercoverage response ID!
-        'user_email': 'chris+user_email@cgsmith.net',
-        'street': '917 Main Street',
+        'website': 'https://www.email.com/En-US/eus/eusdatadetails/1401403', // @todo this is the response ID!
+        'user_email': 'chris+user_email@email.com',
+        'street': '123 Main Street',
         'street2': '',
         'zip': '53149',
         'city': 'Mukwonago',
-        'phone': '262-220-7784',
+        'phone': '555-555-5555',
         'won_status': 'pending',
         'state_id': 10, // @todo need to have a lookup for state IDs
         'display_name': 'Lead from Website'
